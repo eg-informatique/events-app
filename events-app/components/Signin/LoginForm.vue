2e6696a639b824f63e1fcdf98d8dd0f422721095
Paul Lasry-Robin <paul.lasryrobin@gmail.com>
2024-05-14 01:38:52 +0200

    commit

--- events-app/components/Signin/LoginForm.vue
+++ events-app/components/Signin/LoginForm.vue
@@ -1,68 +1,43 @@
 <template>
-    <form class="space-y-4 md:space-y-6" action="#">
-      <div>
-        <label
-          for="email"
-          class="block mb-2 text-sm font-medium text-gray-900"
-          >Email</label
-        >
-        <input
-          type="email"
-          name="email"
-          id="email"
-          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-          placeholder="name@company.com"
-        />
-      </div>
-      <div>
-        <label
-          for="password"
-          class="block mb-2 text-sm font-medium text-gray-900"
-          >Password</label
-        >
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="••••••••"
-          class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-        />
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-start">
-          <div class="flex items-center h-5">
-            <input
-              id="remember"
-              aria-describedby="remember"
-              type="checkbox"
-              class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
-            />
-          </div>
-          <div class="ml-3 text-sm">
-            <label for="remember" class="text-gray-500"
-              >Remember me</label
-            >
-          </div>
+  <form class="space-y-4 md:space-y-6" @submit.prevent="_$event => signIn('credentials', { email, password })">
+    <div>
+      <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
+      <input v-model="email" type="email" name="email" id="email"
+        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+        placeholder="name@company.com" required />
+    </div>
+    <div>
+      <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+      <input v-model="password" type="password" name="password" id="password" placeholder="••••••••"
+        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "/>
+    </div>
+    <div class="flex items-center justify-between">
+      <div class="flex items-start">
+        <div class="flex items-center h-5">
+          <input id="remember" aria-describedby="remember" type="checkbox"
+            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " />
         </div>
-        <a
-          href="#"
-          class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-          >Forgot password?</a
-        >
+        <div class="ml-3 text-sm">
+          <label for="remember" class="text-gray-500">Remember me</label>
+        </div>
       </div>
-      <button
-        type="submit"
-        class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-      >
-        Sign in
-      </button>
-      <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-        Do not have an account yet?
-        <a
-          href="#"
-          class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-          >Sign up</a
-        >
-      </p>
-    </form>
-  </template>
+      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
+        password?</a>
+    </div>
+    <button type="submit"
+      class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+      Sign in
+    </button>
+    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+      Do not have an account yet?
+      <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+    </p>
+  </form>
+</template>
+
+<script setup>
+  const { signIn } = useAuth()
+
+  const email = ref('')
+  const password = ref('')
+</script>
--- events-app/server/api/auth/[...].ts
+++ events-app/server/api/auth/[...].ts
@@ -1,7 +1,6 @@
 import { NuxtAuthHandler } from '#auth'
 import GithubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { useFetch } from 'nuxt/app'
 export default NuxtAuthHandler({
     pages: {
         signIn:'/login'
@@ -16,8 +15,17 @@ export default NuxtAuthHandler({
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         CredentialsProvider.default({
             name: 'Credentials',
-            authorize(credentials: any) {
-                const {data:user} = useFetch('https://events-api.org/login', {method: 'GET', body:{"email":credentials?.email, "password":credentials?.password}})
+            async authorize(credentials: any) {
+                const user = await fetch(`https://events-api.org/login`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        email: credentials?.email,
+                        password: credentials?.password
+                    }),
+                });
+                if (JSON.stringify(user) === 'true'){
+                    return user
+                }
             }
         })
     ]
