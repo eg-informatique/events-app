9a354b920d7f241fdb944f82e650302e5b028c90
Paul <paul.lasryrobin@gmail.com>
2024-05-27 18:33:32 +0200

    Now can acces to the email of the user in frontend with credentials

--- events-app/pages/account.vue
+++ events-app/pages/account.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <h1>Vous etes sur le dash board</h1>
+        <p>{{ data }}</p>
     </div>
 </template>
 
 <script setup>
-
+    const {data } = useAuth()
 </script>
 
 <style lang="scss" scoped>
--- events-app/server/api/auth/[...].ts
+++ events-app/server/api/auth/[...].ts
@@ -15,6 +15,42 @@ export default NuxtAuthHandler({
         signIn:'/login'
     },
     secret: 'polosecrets',
+    providers:[
+        // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+        GithubProvider.default({
+           clientId: 'Ov23liguUeojPL4ky0Gi',
+           clientSecret: '9989e8c5a1098a874c8886d139cc610abbd48ab8'
+        }), 
+        // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
+        CredentialsProvider.default({
+            name: 'Credentials',
+            async authorize(credentials: any) {
+                try{
+                    const user = await fetch(`https://events-api.org/login`, {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            email: credentials?.email,
+                            password: credentials?.password
+                        })
+                    })
+
+                    if(user.ok) {
+                        const response = await fetch(`https://events-api.org/user?email=${credentials?.email}`)
+                        const userData = await response.json()
+                        return{
+                            email: userData.user.email
+                        }
+                    }
+                } catch(error){
+                    console.error('Authentication error:', error)
+                    throw new Error('Authentication error')
+                }
+            }
+        })
+    ],
     callbacks: {
         async signIn({user, account, profile}) {
             if(account?.provider === 'github') {
@@ -48,40 +84,5 @@ export default NuxtAuthHandler({
                 return true
             }
         }
-    },
-    providers:[
-        // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-        GithubProvider.default({
-           clientId: 'Ov23liguUeojPL4ky0Gi',
-           clientSecret: '9989e8c5a1098a874c8886d139cc610abbd48ab8'
-        }), 
-        // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
-        CredentialsProvider.default({
-            name: 'Credentials',
-            async authorize(credentials: any) {
-                try{
-                    const user = await fetch(`https://events-api.org/login`, {
-                        method: "POST",
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            email: credentials?.email,
-                            password: credentials?.password
-                        })
-                    })
-
-                    if(user.ok) {
-                        const userData = await user.json()
-                        return userData
-                    } else {
-                        throw new Error('Failed to authenticate')
-                    }
-                } catch(error){
-                    console.error('Authentication error:', error)
-                    throw new Error('Authentication error')
-                }
-            }
-        })
-    ]
+    }
 })
