6901791a9ce9332348c497cb283afd9391ed239d
Paul Lasry-Robin <paul.lasryrobin@gmail.com>
2024-05-29 20:54:50 +0200

    Changed format of event_details

--- events-app/components/EventDetailsCard.vue
+++ events-app/components/EventDetailsCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <UCard>
+    <UCard class="block sm:hidden">
         <template #header>
             <img class="rounded-md w-[400px] mx-auto " :src="event.img_url">
         </template>
-        <div v-if="event.prices.minor===event.prices.major" class="grid grid-cols-3 grid-rows-1">
+        <div v-if="event.prices.minor===event.prices.major" class="grid grid-cols-3 grid-rows-1 ">
             <p class="text-2xl font-bold mt-3">{{ event.title }}</p>
             <p class="flex font-bold text-gray-400">{{ event.prices.major }} {{ event.prices.currency }}</p>
         </div>
@@ -13,7 +13,40 @@
             
         </div> 
         <p class="font-bold text-gray-400">{{ venueData.name }}</p>
+        <p class="font-bold mt-1.5">{{ $formatShortDate(event, 'start_datetime') }} - {{ $formatShortDate(event, 'end_datetime') }}</p>
+        <div class="w-[3/4] break-words md:break-all">
+           <p class="font-bold ">{{ event.description }}</p> 
+        </div>
+            <UButton square label="Button" class="text-1xl font-bold mt-3" variant="solid">Button</UButton>
     </UCard>
+
+    <UCard class="hidden sm:block">
+        <template #header>
+            <p class="text-2xl font-bold mt-3">{{ event.title }}</p>
+        </template>
+            <div class="flex items-start space-x-7">
+                <img class="rounded-md w-[400px] " :src="event.img_url">
+                <p class="font-bold w-[1] flex-grow break-words md:break-all">{{ event.description }}</p>
+                <USkeleton class="h-[300px] w-[450px] margin-left: auto;" />
+            </div>
+            
+        <p class="font-bold text-gray-400">{{ venueData.name }}</p>
+        <p class="font-bold mt-1.5">{{ $formatShortDate(event, 'start_datetime') }} - {{ $formatShortDate(event, 'end_datetime') }}</p>
+        <div class="w-[3/4] break-words md:break-all">
+        </div>
+        <div v-if="event.prices.minor===event.prices.major" class=" grid-rows-1 ">
+            <p class="text-2xl font-bold mt-3">{{ event.title }}</p>
+            <p class="flex font-bold text-gray-400">{{ event.prices.major }} {{ event.prices.currency }}</p>
+        </div>
+        <div v-else class="grid grid-cols-3 grid-rows-1 ">
+            <p class="font-bold text-gray-400 mt-1.5">{{ event.prices.minor }} - {{ event.prices.major }} {{ event.prices.currency }}</p>
+            
+        </div> 
+        <template #footer>
+            <UButton square label="Button" class="text-1xl font-bold mt-3" variant="solid">Reserve</UButton>
+        </template>
+    </UCard>
+
 
     <!--Pettttttre, la documentation de <Ucard> ==> https://ui.nuxt.com/components/card et les champs de l'api (evenement) ==> https://events-api.org/events
         Si tu veux utiliser un champs de l'api tu fais comme au dessus: event.title par ex. Si tu veuc l'ajouter a de l'HTML comme au dessus tu
@@ -25,6 +58,7 @@
 </template>
 
 <script setup>
+    const { $formatShortDate } = useNuxtApp()
     const { event } = defineProps(['event'])
     const venue = await fetch(`https://events-api.org/venue/${event.venue}`)
     const venueData = await venue.json()
