b8e5b769d71a07d41777891b954c8cf76adb6531
Paul Lasry-Robin <paul.lasryrobin@gmail.com>
2024-08-02 14:57:15 +0200

    Search Bar visual added no function yet

--- events-app/pages/index.vue
+++ events-app/pages/index.vue
@@ -1,29 +1,43 @@
 <template>
-    <div class="parent-container">
-        <div class="right-06 mb-3">
-            <UPopover mode="hover" :popper="{ placement: 'top-end' }">
-                <UButton color="white" :label="$t('home_sort_by_btn')" trailing-icon="i-heroicons-chevron-down-20-solid" />
-                <template #panel="{ close }">
-                    <div class="button-container mb-3">
-                        <UButton :label="$t('home_datesort_asc')" @click="() => changeSortOrder($t('home_datesort_asc'), 'ascending', close)" />
-                    </div>
-                    <div class="button-container">
-                        <UButton :label="$t('home_datesort_desc')" @click="() => changeSortOrder($t('home_datesort_desc'), 'descending', close)" />
-                    </div>
-                </template>
-            </UPopover>
-        </div>
-    </div>
-
+     
     <div v-if="pending" class="flex flex-col items-center justify-center h-full">
         <Icon name="line-md:loading-twotone-loop" size="80px"/>
     </div>
     <div v-else-if="error" class="flex flex-col items-center justify-center h-full">
         <p class="text-red-500 font-bold">{{ $t('index_loading_error') }}</p>
     </div>
-    <div v-else class="grid lg:grid-cols-4 gap-7">
-        <div v-for="event in sortedEvents" :key="event.id">
+    <div v-else >
+        <div class="parent-container" flex items-end>
+                <div class="flex justify-between items-center mb-3 w-full " >
+                     <UInput
+                        icon="i-heroicons-magnifying-glass-20-solid"
+                        :placeholder="$t('search_events')"
+                        class="search-bar"
+                        variant="none"
+                        size="xl"
+                        
+                        >
+                    </UInput>
+                </div>
+                <div class="right-06 mb-3" >
+                    <UPopover mode="hover" :popper="{ placement: 'top-end' }">
+                        <UButton color="white" :label="$t('home_sort_by_btn')" trailing-icon="i-heroicons-chevron-down-20-solid" />
+                        <template #panel="{ close }">
+                            <div class="button-container mb-3">
+                                <UButton :label="$t('home_datesort_asc')" @click="() => changeSortOrder($t('home_datesort_asc'), 'ascending', close)" />
+                          </div>
+                            <div class="button-container">
+                             <UButton :label="$t('home_datesort_desc')" @click="() => changeSortOrder($t('home_datesort_desc'), 'descending', close)" />
+                         </div>
+                      </template>
+                  </UPopover>
+               </div>
+           </div>
+           <div class="grid lg:grid-cols-4 gap-7">     
+            <div v-for="event in sortedEvents" :key="event.id" >
             <EventCard :event="event" />
+        </div>
+
         </div>
     </div>
 </template>
@@ -34,7 +48,7 @@ import { ref, computed } from 'vue';
 const { data: events, pending, error } = useLazyFetch('/api/fetchEvents');
 definePageMeta({ auth: false });
 
-const buttonLabel = ref("Sort By"); // ne marche pas encore, je narrive pas a le traduire pour le moment.
+const buttonLabel = ref("Sort By"); 
 const sortOrder = ref('ascending');
 
 const sortedEvents = computed(() => {
@@ -62,13 +76,24 @@ const changeSortOrder = (newLabel, order, close) => {
 <style scoped>
 .parent-container {
     display: flex; 
+    align-items: flex-end;
 }
 
 .right-06 {
     margin-left: auto;  
+    
 }
 
 .button-container {
-    padding: 0px 0;  
+    padding: 0px 0; 
+ }
+
+    .search-bar {
+  width: 100%;
+  max-width: 700px; 
+  padding: 10px 20px;
+  border-radius: 30px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
 }
+
 </style>
