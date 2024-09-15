32096e63159e8af187f929d01a1310b2a401b3b1
Paul <paul.lasryrobin@gmail.com>
2024-09-15 22:28:28 +0200

    Begin the new sidebar

---
+++ events-app/components/CreateUser/GoogleSignUp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { signIn } = useAuth()
+</script>
+
+<template>
+  <UButton 
+  :label="$t('signup_google')"
+  icon="i-mdi-google"
+  variant="outline"
+  @click="signIn('google')"
+  />
+</template>
