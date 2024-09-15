58154fb6fe00dcee93b16cca23807560eca5af3f
Paul <paul.lasryrobin@gmail.com>
2024-09-15 22:28:41 +0200

    begin the new Sidebar

--- events-app/components/Sidebar/ProfileDropDown.vue
+++ events-app/components/Sidebar/ProfileDropDown.vue
@@ -1,6 +1,6 @@
 <template>
-  <UDropdown :popper="{ placement: 'bottom-start' } ":ui="{ item: { disabled: 'cursor-text select-text' } }":items="items" class="w-10 h-10 mb-5">
-    <UAvatar icon="i-heroicons-user" class="w-10 h-10"/>
+  <UDropdown v-if="logedIn" :popper="{ placement: 'bottom-start' } ":ui="{ item: { disabled: 'cursor-text select-text' } }":items="items" class="w-10 h-10 mb-5 mr-2">
+    <UAvatar icon="i-mdi-account" class="w-10 h-10"/>
 
     <template #account="{ item }">
       <div class="text-left">
@@ -19,18 +19,30 @@
       <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
     </template>
   </UDropdown>
-
+  <UButton v-else 
+    @click="handleSignIn"
+    :label="$t('signin_nav_btn')"
+    icon="i-mdi-sign-in"
+    class="ml-2 mt-1"
+  />
 </template>
 
 <script setup lang="ts">
+import { icons } from '@iconify-json/majesticons/index.js';
+
 
 const { t } = useI18n()
-const { status, signIn, signOut } = useAuth()
+const { status, signIn, signOut, data } = useAuth()
 const locaPath = useLocalePath()
+const logedIn = computed(() => status.value === 'authenticated')
 
-
-
-const items = [
+async function handleSignIn() {
+    await signIn()
+}
+async function handleSignOut() {
+    await signOut()
+}
+const item = [
   [{
     label: 'ben@example.com',
     slot: 'account',
@@ -38,7 +50,8 @@ const items = [
   }], [{
     label: 'Settings',
     icon: 'i-heroicons-cog-8-tooth'
-  }], [{
+  }], 
+  [{
     label: 'Documentation',
     icon: 'i-heroicons-book-open'
   }, {
@@ -50,6 +63,56 @@ const items = [
   }], [{
     label: 'Sign out',
     icon: 'i-heroicons-arrow-left-on-rectangle'
+  }],
+  [{
+    label: 'Sign in',
+    icon: 'i-heroicons-arrow-left-on-rectangle'
   }]
 ]
+
+const items = [
+  [{
+    label:t('acount_nav'),
+    to:`${locaPath('/account')}`,
+    icon:'i-mdi-account'
+  },
+  {
+    label:t('reservations_nav'),
+    to:`${locaPath('/reservations')}`,
+    icon:'i-mdi-music'
+  },
+  {
+    label:t('myevents_nav'),
+    to:`${locaPath('/myevents')}`,
+    icon:'i-mdi-plus'
+  },
+  ],
+  [
+    {
+      label:t('signout_nav_btn'),
+      icon:'i-mdi-sign-out',
+      click: async () => {
+        await signOut()
+      }
+    }
+  ]
+]
+
+const itemsSignedIn = ref([
+    {
+        label: t('home_nav'),
+        to: `${locaPath('/')}`,
+        icon: "i-majesticons-home"
+    },
+    {
+        label : t('about_us_nav'),
+        to: `${locaPath('/about')}`,
+        icon: "i-mdi-about-circle-outline"
+    },
+    {
+        label:t('acount_nav'),
+        to:`${locaPath('/account')}`,
+        icon:'i-mdi-account'
+    }
+])
 </script>
