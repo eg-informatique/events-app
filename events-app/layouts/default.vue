934475ad6db73253ad42618b23abac1378d9adf7
polo20069 <89207651+polo20069@users.noreply.github.com>
2024-09-19 19:36:23 +0200

    Update default.vue

--- events-app/layouts/default.vue
+++ events-app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="grid lg:flex min-h-screen">
     <Sidebar />
     <div class="flex flex-col w-full">
-      <div class="flex-grow p-5 overflow-auto">
+      <div class="flex-grow p-5 overflow-auto mt-20">
         <slot />
       </div>
       <Footer />
