d0586229b67903d39445d92051a0ad6fcd125b64
polo20069 <89207651+polo20069@users.noreply.github.com>
2024-10-09 12:27:06 +0200

    Update default.vue

--- events-app/layouts/default.vue
+++ events-app/layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex flex-col min-h-screen w-full overflow-x-hidden">
-    <div class="w-full p-5">
+  <div class="flex flex-col min-h-screen w-full overflow-hidden">
+    <div class="w-full p-5 lg:px-16">
       <Sidebar />
+
       <div class="flex-grow overflow-auto lg:pt-16 w-full">
         <slot />
       </div>
